2170be4b61ab30069be154ce88dff2a1ea111c00
lugDroid <lugdroid@gmail.com>
2016-03-16 22:45:55 +0000

    Refactoring as object module

--- js/main.js
+++ js/main.js
@@ -1,36 +1,51 @@
-$(document).ready(function() {
-  var template = $("#news-template").html();
-
-  console.log("waiting for data");
-  $.getJSON("http://www.freecodecamp.com/news/hot", function(data) {
-    console.log("data received");
-    var index = 0;
-
-    for (var i = 0; i < (data.length / 3) + 1; i++) {
+var news = {
+  template: $("#news-template").html(),
+  news: [],
+  index: 0,
+  getData: function(data) {
+    for (var i = 0; i < data.length; i++) {
+      var date = new Date(data[this.index].timePosted);
+      var news = {
+        headline: data[this.index].headline.slice(0, 50) + "...",
+        link: data[this.index].link,
+        username: data[this.index].author.username,
+        timePosted: date.getDate() + "/" + date.getMonth() + "/" + date.getYear(),
+        rank: data[this.index].rank
+      };
+      if (data[this.index].image !== "") {
+        news.image = data[this.index].image;
+      } else {
+        news.image = data[this.index].author.picture;
+      }
+      this.news.push(news);
+    }
+  },
+  init: function() {
+    this.cacheDom();
+    this.getNews();
+    this.render();
+  },
+  cacheDom: function() {
+    this.$wrapper = $(".site-wrapper");
+  },
+  getNews: function() {
+    $.getJSON("http://www.freecodecamp.com/news/hot", this.getData.bind(this));
+  },
+  render: function() {
+    for (var i = 0; i < (this.news.length / 3) + 1; i++) {
       $(".site-wrapper").append("<div class='row' id='row-" + i + "'></div>");
-
       for (var j = i; j < (i + 3); j++) {
-        var date = new Date(data[index].timePosted);
-
-        var news = {
-          headline: data[index].headline.slice(0, 50) + "...",
-          //image: "",
-          link: data[index].link,
-          username: data[index].author.username,
-          timePosted: date.getDate() + "/" + date.getMonth() + "/" + date.getYear(),
-          rank: data[index].rank
+        var data = {
+          news: this.news[j]
         };
-
-        if (data[index].image !== "") {
-          news.image = data[index].image;
-        } else {
-          news.image = data[index].author.picture;
-        }
-
-        $("#row-" + i).append(Mustache.to_html(template, news));
-
-        index++;
+        console.log(data);
+        $("#row-" + i).append(Mustache.render(this.template, data));
+        this.index++;
       }
     }
-  });
+  }
+};
+
+$(document).ready(function() {
+  news.init();
 });
