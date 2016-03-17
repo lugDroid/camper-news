a75b68e8a608e29ad0037cb239cc090f339ae5a2
lugDroid <lugdroid@gmail.com>
2016-03-17 19:18:18 +0000

    Freecodecamp JSONP not working

--- js/main.js
+++ js/main.js
@@ -1,48 +1,52 @@
 var news = {
-  template: $("#news-template").html(),
-  news: [],
+  articlesList: [],
   index: 0,
+  article: {},
+  cacheDom: function() {
+    this.$wrapper = $(".site-wrapper");
+    this.template = $("#news-template").html();
+  },
+  getNews: function() {
+    console.log("Getting data...");
+    $.getJSON("http://www.freecodecamp.com/news/hot?callback=?", this.getData.bind(this));
+  },
   getData: function(data) {
+    console.log("Parsing data received...");
+    var date;
     for (var i = 0; i < data.length; i++) {
-      var date = new Date(data[this.index].timePosted);
-      var news = {
-        headline: data[this.index].headline.slice(0, 50) + "...",
-        link: data[this.index].link,
-        username: data[this.index].author.username,
+      date = new Date(data[i].timePosted);
+      this.article = {
+        headline: data[i].headline.slice(0, 50) + "...",
+        link: data[i].link,
+        username: data[i].author.username,
         timePosted: date.getDate() + "/" + date.getMonth() + "/" + date.getYear(),
-        rank: data[this.index].rank
+        rank: data[i].rank
       };
-      if (data[this.index].image !== "") {
-        news.image = data[this.index].image;
+      if (data[i].image !== "") {
+        this.article.image = data[i].image;
       } else {
-        news.image = data[this.index].author.picture;
+        this.article.image = data[i].author.picture;
       }
-      this.news.push(news);
+      this.articlesList.push(this.article);
+    }
+  },
+  render: function() {
+    for (var i = 0; i < (this.articlesList.length / 3) + 1; i++) {
+      $(".site-wrapper").append("<div class='row' id='row-" + i + "'></div>");
+      for (var j = i; j < (i + 3); j++) {
+        var data = {
+          news: this.articlesList[j]
+        };
+        $("#row-" + i).append(Mustache.render(this.template, data));
+        this.index++;
+      }
     }
   },
   init: function() {
     this.cacheDom();
     this.getNews();
+    console.log(this.articlesList);
     this.render();
-  },
-  cacheDom: function() {
-    this.$wrapper = $(".site-wrapper");
-  },
-  getNews: function() {
-    $.getJSON("http://www.freecodecamp.com/news/hot", this.getData.bind(this));
-  },
-  render: function() {
-    for (var i = 0; i < (this.news.length / 3) + 1; i++) {
-      $(".site-wrapper").append("<div class='row' id='row-" + i + "'></div>");
-      for (var j = i; j < (i + 3); j++) {
-        var data = {
-          news: this.news[j]
-        };
-        console.log(data);
-        $("#row-" + i).append(Mustache.render(this.template, data));
-        this.index++;
-      }
-    }
   }
 };
 
